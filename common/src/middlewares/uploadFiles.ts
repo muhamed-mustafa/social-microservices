361e1189716c281dbacd8909f9f9694c543812e2
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2022-02-05 14:12:19 +0200

    Updates

--- common/src/middlewares/uploadFiles.ts
+++ common/src/middlewares/uploadFiles.ts
@@ -9,37 +9,25 @@ const validationPhoto = (req : Request , res : Response , next : NextFunction) =
 {
     const files = req.files as { [fieldname : string] : Express.Multer.File[] };
 
-    if(files.profilePicture)
+    const images = ['image' , 'profilePicture' , 'coverPicture'];
+    images.forEach(img =>
     {
-        files.profilePicture.map(file =>
+        if(files[img])
         {
-            if(!file.originalname.match(/\.(jpg|jpeg|png)/))
+            files.img.map(file =>
             {
-                throw new BadRequestError(`${file.originalname} should be a valid image.`);
-            }
+                if(!file.originalname.match(/\.(jpg|jpeg|png)/))
+                {
+                    throw new BadRequestError(`${file.originalname} should be a valid image.`);
+                }
 
-            if(file.size > 1e6)
-            {
-                throw new BadRequestError(`${file.size} is Larger`);
-            }
-        });
-    };
-
-    if(files.coverPicture)
-    {
-        files.coverPicture.map(file =>
-        {
-            if(!file.originalname.match(/\.(jpg|jpeg|png)/))
-            {
-                throw new BadRequestError(`${file.originalname} should be a valid image.`);
-            }
-
-            if(file.size > 1e6)
-            {
-                throw new BadRequestError(`${file.size} is Larger`);
-            }
-        });
-    };
+                if(file.size > 1e6)
+                {
+                    throw new BadRequestError(`${file.size} is Larger`);
+                }
+            });
+        };
+    });
 
     next();
 }
